886a313b1ffe62a4083694e5480777c4fa1145a0
Andrii Holovchak <andrii.holovchak@yalantis.net>
2025-02-12 21:59:49 +0200

    base setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb://mongo:cmBcNKffBYsxGghHCMoeMMNHEEsgxRRX@roundhouse.proxy.rlwy.net:31720',
+      {
+        auth: {
+          username: 'mongo',
+          password: 'cmBcNKffBYsxGghHCMoeMMNHEEsgxRRX',
+        },
+      },
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
