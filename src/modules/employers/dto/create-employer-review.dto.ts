bbccc1b2d7f58d063def5dbee9d8402ed0067c62
Andrii Holovchak <andrii.holovchak@yalantis.net>
2025-02-26 00:14:28 +0200

    user reviews

--- src/modules/employers/dto/create-employer-review.dto.ts
+++ src/modules/employers/dto/create-employer-review.dto.ts
@@ -2,5 +2,5 @@ import { OmitType } from '@nestjs/swagger';
 import { CreateReviewDto } from '../../reviews/dto/create-review.dto';
 
 export class CreateEmployerReviewDto extends OmitType(CreateReviewDto, [
-  'employerId',
+  'employer',
 ] as const) {}
